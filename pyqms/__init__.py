7ce175182535db58800c63e39a02bb19c7d837d6
Manuel <m_koes17@uni-muenster.de>
2019-05-20 17:14:14 +0200

    Remove unused version in pyQms/__init__.pyQms
    - remove unused variable version_info hardcoded to (0, 5, 0, 'beta')
    - remove unused variable version hardcoded to 0.5.0-beta
    - update RuntimeError warning to print correct required version

--- pyqms/__init__.py
+++ pyqms/__init__.py
@@ -21,11 +21,9 @@
 """
 from __future__ import absolute_import
 import sys
-version_info  = (0, 5, 0, 'beta')
-version = '0.5.0-beta'
 
 if not hasattr(sys, "version_info") or sys.version_info < (3, 5):
-    raise RuntimeError("pyQms requires Python 3.3 or later.")
+    raise RuntimeError("pyQms requires Python 3.5 or later.")
 
 from . import knowledge_base
 from .isotopologue_library import IsotopologueLibrary
